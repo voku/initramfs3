524a913b43b6ca6e0dae8c1cda4c43f698a39933
Dorimanx <yuri@bynet.co.il>
2012-04-11 16:19:38 +0300

    Tuned for ZRAM activation and added swapon/off -p busybox bin Now i have 3 ZRAM of 100MB in the same priority :)

--- sbin/ext/modules.sh
+++ sbin/ext/modules.sh
@@ -19,5 +19,26 @@ insmod /lib/modules/Si4709_driver.ko
 # for ntfs automounting
 insmod /lib/modules/fuse.ko
 # For ZRAM auto load
+insmod /lib/modules/lzo_compress.ko
+insmod /lib/modules/lzo_decompress.ko
 insmod /lib/modules/zram.ko num_devices=3
-
+# Now we load the ZRAM as RAM SWAP and gain 150MB more 
+if [ -e /dev/block/zram0 ]; then
+	# Setting swappines
+	echo 60 > /proc/sys/vm/swappiness 
+	# Setting size of each ZRAM swap drives
+	echo 100000000 > /sys/block/zram0/disksize
+	echo 100000000 > /sys/block/zram1/disksize
+        echo 100000000 > /sys/block/zram2/disksize
+	# Creating SWAPS from ZRAM drives
+	mkswap /dev/block/zram0 >/dev/null
+	mkswap /dev/block/zram1 >/dev/null
+	mkswap /dev/block/zram2 >/dev/null
+	# Activating ZRAM swaps with the same priority to load balance ram swapping (need advanced busybox with swapon -p flag)
+	swapon /dev/block/zram0 -p 20 >/dev/null 2>&1
+	swapon /dev/block/zram1 -p 20 >/dev/null 2>&1
+	swapon /dev/block/zram2 -p 20 >/dev/null 2>&1
+	# Show to user that swap is ON
+	free
+	sleep 2
+fi
