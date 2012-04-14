49b5e00d78742ad6fcc41615759415d9ea137b0b
Lars Moelleken <voku@voku-online.de>
2012-04-14 15:17:23 +0200

    + testing

--- sbin/ext/modules.sh
+++ sbin/ext/modules.sh
@@ -11,13 +11,35 @@ chmod 777 /data/.siyah
 read_defaults
 read_config
 
-# We need logs. no logs no Android simple debuging.
-insmod /lib/modules/logger.ko
+#For some reason the  in not loaded! so i load it here.
+insmod /lib/modules/j4fs.ko
+mount -t j4fs /dev/block/mmcblk0p4 /mnt/.lfs
+
+# reduce logcat priority.
 renice 10 `pgrep logcat`
 #fm radio, I have no idea why it isn't loaded in init -gm
 insmod /lib/modules/Si4709_driver.ko
 # for ntfs automounting
 insmod /lib/modules/fuse.ko
 # For ZRAM auto load
-insmod /lib/modules/zram.ko num_devices=3
-
+# insmod /lib/modules/zram.ko num_devices=3 (loading at kernel init.)
+# Now we load the ZRAM as RAM SWAP and gain 150MB more compressed RAM.
+# ZRAM compress ratio is 50% so 300MB will give clean 150MB More RAM, this gives us 1GB RAM device. 
+if [ -e /dev/block/zram0 ]; then
+	# Setting swappines
+	echo 60 > /proc/sys/vm/swappiness 
+	# Setting size of each ZRAM swap drives
+	echo 100000000 > /sys/block/zram0/disksize
+	echo 100000000 > /sys/block/zram1/disksize
+        echo 100000000 > /sys/block/zram2/disksize
+	# Creating SWAPS from ZRAM drives
+	mkswap /dev/block/zram0 >/dev/null
+	mkswap /dev/block/zram1 >/dev/null
+	mkswap /dev/block/zram2 >/dev/null
+	# Activating ZRAM swaps with the same priority to load balance ram swapping (need advanced busybox with swapon -p flag)
+	swapon /dev/block/zram0 -p 20 >/dev/null 2>&1
+	swapon /dev/block/zram1 -p 20 >/dev/null 2>&1
+	swapon /dev/block/zram2 -p 20 >/dev/null 2>&1
+	# Show to user that swap is ON
+	free
+fi
