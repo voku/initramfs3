a1f590fcccce1a03aa989a698db6685200162c04
Dorimanx <yuri@bynet.co.il>
2012-04-15 23:25:51 +0300

    fix thunderbolt.sh location

--- sbin/ext/tweaks.sh
+++ sbin/ext/tweaks.sh
@@ -27,10 +27,9 @@ if [ "$logger" == "off" ];then
   echo 0 > /sys/module/xt_qtaguid/parameters/debug_mask
 fi
 
-#thanks to pikachu01@XDA and Dorimanx
-/sbin/busybox sh /sbin/siyah/thunderbolt.sh
+#thanks to pikachu01@XDA ,Dorimanx@XDA and ELITE Developer Gokhanmoral
+/sbin/busybox sh /sbin/ext/thunderbolt.sh
 mount -o remount,rw,noatime,nodiratime,nodev,nobh,nouser_xattr,inode_readahead_blks=10,discard,barrier=0,commit=60,noauto_da_alloc,delalloc /cache /cache;
 mount -o remount,rw,noatime,nodiratime,nodev,nobh,nouser_xattr,inode_readahead_blks=10,discard,barrier=0,commit=60,noauto_da_alloc,delalloc /data /data;
 mount -o remount,rw,noatime,nodiratime /system /system
-
 
