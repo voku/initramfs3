c727136d2e06d3e56844d241a58bd0ae00ad3c36
Dorimanx <yuri@bynet.co.il>
2012-04-18 11:03:08 +0300

    Time to tweak this baby :)

--- sbin/ext/thunderbolt.sh
+++ sbin/ext/thunderbolt.sh
@@ -4,6 +4,7 @@
 # zacharias.maladroit
 # voku1987
 # collin_ph@xda
+# Dorimanx@xda
 # TAKE NOTE THAT LINES PRECEDED BY A "#" IS COMMENTED OUT!
 
 # ==============================================================
@@ -14,94 +15,59 @@
 # ==============================================================
 # ==============================================================
 
-STL=`ls -d /sys/block/stl*`;
-BML=`ls -d /sys/block/bml*`;
+DM=`ls -d /sys/block/dm*`;
+LOOP=`ls -d /sys/block/loop*`;
 MMC=`ls -d /sys/block/mmc*`;
 ZRM=`ls -d /sys/block/zram*`;
-MTD=`ls -d /sys/block/mtd*`;
+RAM=`ls -d /sys/block/ram*`;
 
-# Optimize non-rotating storage; 
-for i in $STL $BML $MMC $ZRM $MTD;
+# Optimize storage drives and ram drives; 
+for i in $DM $LOOP $MMC $ZRM $RAM;
 do
 	#IMPORTANT!
 	if [ -e $i/queue/rotational ]; 
 	then
-		echo 0 > $i/queue/rotational; 
+		echo "0" > $i/queue/rotational;
 	fi;
-	if [ -e $i/queue/nr_requests ];
-	then
-		echo 1024 > $i/queue/nr_requests; # for starters: keep it sane
-	fi;
-	#CFQ specific
-	if [ -e $i/queue/iosched/back_seek_penalty ];
-	then 
-		echo 1 > $i/queue/iosched/back_seek_penalty;
-	fi;
-	#CFQ specific
-	if [ -e $i/queue/iosched/low_latency ];
-	then
-		echo 1 > $i/queue/iosched/low_latency;
-	fi;
-	#CFQ Specific
-	if [ -e $i/queue/iosched/slice_idle ];
-	then 
-		echo 0 > $i/queue/iosched/slice_idle; # previous: 1
-	fi;
-	# deadline/VR/SIO scheduler specific
-	if [ -e $i/queue/iosched/fifo_batch ];
-	then
-		echo 1 > $i/queue/iosched/fifo_batch;
-	fi;
-	if [ -e $i/queue/iosched/writes_starved ];
-	then
-		echo 1 > $i/queue/iosched/writes_starved;
-	fi;
-	#CFQ specific
-	if [ -e $i/queue/iosched/quantum ];
-	then
-		echo 8 > $i/queue/iosched/quantum;
-	fi;
-	#VR Specific
-	if [ -e $i/queue/iosched/rev_penalty ];
-	then
-		echo 1 > $i/queue/iosched/rev_penalty;
-	fi;
-	if [ -e $i/queue/rq_affinity ];
-	then
-		echo "1" > $i/queue/rq_affinity;   
-	fi;
-#disable iostats to reduce overhead  # idea by kodos96 - thanks !
+
 	if [ -e $i/queue/iostats ];
 	then
 		echo "0" > $i/queue/iostats;
 	fi;
+
+	if [ -e $i/queue/nr_requests ];
+	then
+		echo "1024" > $i/queue/nr_requests; # for starters: keep it sane
+	fi;
+
+	if [ -e $i/queue/iosched/low_latency ];
+	then
+		echo "1" > $i/queue/iosched/low_latency;
+	fi;
+
+	#CFQ Specific
+	if [ -e $i/queue/iosched/slice_idle ];
+	then 
+		echo "0" > $i/queue/iosched/slice_idle; # previous: 1
+	fi;
+
+	if [ -e $i/queue/rq_affinity ];
+	then
+		echo "1" > $i/queue/rq_affinity;   
+	fi;
+
 # Optimize for read- & write-throughput; 
 # Optimize for readahead; 
 	if [ -e $i/queue/read_ahead_kb ];
 	then
-		echo "256" >  $i/queue/read_ahead_kb;
+		echo "512" >  $i/queue/read_ahead_kb;
 	fi;
-# Commented out nomerges (Merges are good). max_sectors_kb to default - don't tweak it
-#          echo "0"   >  $i/queue/nomerges
-#          echo "128" >  $i/queue/max_sectors_kb
-	
+
 done;
-
-
 
 # =========
 # TWEAKS: raising read_ahead_kb cache-value for mounts that are sdcard-like to 1024 
 # =========
-
-#if [ -e /sys/devices/virtual/bdi/179:0/read_ahead_kb ];
-#then
-#    echo "1024" > /sys/devices/virtual/bdi/179:0/read_ahead_kb;
-#fi;
-	
-#if [ -e /sys/devices/virtual/bdi/179:8/read_ahead_kb ];
-#  then
-#    echo "1024" > /sys/devices/virtual/bdi/179:8/read_ahead_kb;
-#fi;
 
 if [ -e /sys/devices/virtual/bdi/179:16/read_ahead_kb ];
   then
@@ -118,7 +84,6 @@ if [ -e /sys/devices/virtual/bdi/default/read_ahead_kb ];
     echo "512" > /sys/devices/virtual/bdi/default/read_ahead_kb;
 fi;
 
-
 # ==============================================================
 # ==============================================================
 # ==============================================================
@@ -127,11 +92,231 @@ fi;
 # ==============================================================
 # ==============================================================
 
-
 # Remount all partitions with noatime and nodiratime
 for k in $(busybox mount | grep relatime | cut -d " " -f3);
 do
-#sync;
-busybox mount -o remount,rw,noatime,nodiratime $k;
+	busybox mount -o remount,rw,noatime,nodiratime $k;
 done;
 
+# =========
+# TWEAKS
+# =========
+echo "0" > /proc/sys/vm/oom_kill_allocating_task;
+setprop ro.media.dec.jpeg.memcap 20000000;
+setprop ro.media.enc.jpeg.quality 100,100,100;
+
+# =========
+# MEMORY-TWEAKS
+# =========
+echo "25" > /proc/sys/vm/vfs_cache_pressure;
+echo "4" > /proc/sys/vm/min_free_order_shift;
+echo "0" > /proc/sys/vm/overcommit_memory;
+echo "96 96" > /proc/sys/vm/lowmem_reserve_ratio;
+echo "1" > /proc/sys/vm/page-cluster;
+echo "1000" > /proc/sys/vm/overcommit_ratio;
+
+# =========
+# FS-TWEAKS
+# =========
+echo "10" > /proc/sys/fs/lease-break-time;
+
+# =========
+# KERNEL-TWEAKS
+# =========
+echo "Android-VM" > /proc/sys/kernel/hostname;
+echo "0" > /proc/sys/kernel/hung_task_panic;
+
+# Define the memory thresholds at which the above process classes will
+# be killed. These numbers are in pages (4k) -> (1 MB * 1024) / 4 = 256
+#FOREGROUND_APP_MEM=8192;
+#VISIBLE_APP_MEM=10240;
+#SECONDARY_SERVER_MEM=12288;
+#BACKUP_APP_MEM=12288;
+#HOME_APP_MEM=12288;
+#HIDDEN_APP_MEM=14336;
+#CONTENT_PROVIDER_MEM=16384;
+#EMPTY_APP_MEM=20480;
+#echo "$FOREGROUND_APP_MEM,$VISIBLE_APP_MEM,$SECONDARY_SERVER_MEM,$HIDDEN_APP_MEM,$CONTENT_PROVIDER_MEM,$EMPTY_APP_MEM" > /sys/module/lowmemorykiller/parameters/minfree;
+
+# =========
+# CPU - Tweaks
+# =========
+#echo "conservative" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+#echo "10000000" > /proc/sys/kernel/sched_latency_ns;
+#echo "1000000" > /proc/sys/kernel/sched_wakeup_granularity_ns;
+#echo "800000" > /proc/sys/kernel/sched_min_granularity_ns;
+#echo "-1" > /proc/sys/kernel/sched_rt_runtime_us;
+#echo "100000" > /proc/sys/kernel/sched_rt_period_us;
+
+# =========
+# Renice - kernel thread responsible for managing the memory
+# =========
+renice 15 `pidof kswapd0`;
+renice -17 `pgrep htcleo_touch_wq`
+
+# =========
+# CleanUp
+# =========
+#drop caches to free some memory
+#sync;
+#echo "3" > /proc/sys/vm/drop_caches;
+#sleep 1;
+#echo "1" > /proc/sys/vm/drop_caches; 
+
+# =========
+# Explanations
+# =========
+# scaling_governor: Using Frequency Scaling Governors -> cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_governors
+#
+# 				-> http://publib.boulder.ibm.com/infocenter/lnxinfo/v3r0m0/index.jsp?topic=/liaai/cpufreq/TheOndemandGovernor.htm
+#
+# 				conservative - Increases frequency step by step, decreases instantly
+# 				ondemand - Uses the highest CPU frequency when tasks are started, decreases step by step
+# 				performance - CPU only runs at max frequency regardless of load
+# 				powersave - CPU only runs at min frequency regardless of load
+#
+# 				/system/xbin/echo "ondemand" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+
+# swappiness: 	swappiness is a parameter which sets the kernel's balance between reclaiming pages from the page cache and swapping process memory. 
+#				The default value is 60. If you want kernel to swap out more process memory and thus cache more file contents increase the value. 
+#				Otherwise, if you would like kernel to swap less decrease it. A value of 0 means "do not swap unless out of free RAM", 
+#				a value of 100 means "swap whenever possible". 
+#
+# 				/system/xbin/echo XXX > /proc/sys/vm/swappiness;
+
+# oom_kill_allocating_task:	If this is set to zero, the OOM killer will scan through the entire tasklist and select a task based on heuristics to kill. 
+#				This normally selects a rogue memory-hogging task that frees up a large amount of memory when killed. 
+#				If this is set to non-zero, the OOM killer simply kills the task that triggered the out-of-memory condition. 
+#				This avoids the expensive tasklist scan.
+#
+# 				/system/xbin/echo XXX > /proc/sys/vm/oom_kill_allocating_task;
+
+# dirty_expire_centisecs: This tunable is used to define when dirty data is old enough to be eligible for writeout by the pdflush daemons. 
+#				It is expressed in 100'ths of a second. Data which has been dirty in memory for longer than this interval will be written 
+#				out next time a pdflush daemon wakes up.
+#
+# 				/system/xbin/echo XXX > /proc/sys/vm/dirty_expire_centisecs;
+
+# dirty_writeback_centisecs: The pdflush writeback daemons will periodically wake up and write "old" data out to disk. 
+#				This tunable expresses the interval between those wakeups, in 100'ths of a second. 
+#				Setting this to zero disables periodic writeback altogether.
+#
+# 				/system/xbin/echo XXX > /proc/sys/vm/dirty_writeback_centisecs;
+
+# drop_caches:	Writing to this will cause the kernel to drop clean caches, dentries and inodes from memory, causing that memory to become free.
+#
+#				To free pagecache:
+# 				/system/xbin/echo 1 > /proc/sys/vm/drop_caches
+#
+#				To free dentries and inodes:
+# 				/system/xbin/echo 2 > /proc/sys/vm/drop_caches
+#
+#				To free pagecache, dentries and inodes:
+# 				/system/xbin/echo 3 > /proc/sys/vm/drop_caches
+
+# page-cluster: page-cluster controls the number of pages which are written to swap in a single attempt. The swap I/O size.
+#				It is a logarithmic value - setting it to zero means "1 page", setting it to 1 means "2 pages", setting it to 2 means "4 pages", etc.
+#				The default value is three (eight pages at a time). There may be some small benefits in tuning this to 
+#				a different value if your workload is swap-intensive. (default 3)
+#
+# 				/system/xbin/echo XXX > /proc/sys/vm/page-cluster;
+
+# laptop_mode: 	laptop_mode is a knob that controls "laptop mode". When the knob is set, any physical disk I/O 
+#				(that might have caused the hard disk to spin up, see /proc/sys/vm/block_dump) causes Linux to flush all dirty blocks. 
+#				The result of this is that after a disk has spun down, it will not be spun up anymore to write dirty blocks, 
+#				because those blocks had already been written immediately after the most recent read operation. 
+#				The value of the laptop_mode knob determines the time between the occurrence of disk I/O and when the flush is triggered. 
+#				A sensible value for the knob is 5 seconds. Setting the knob to 0 disables laptop mode.
+#
+# 				/system/xbin/echo XXX > /proc/sys/vm/laptop_mode;
+
+# rr_interval: 	rr_interval or "round robin interval". This is the maximum time two SCHED_OTHER (or SCHED_NORMAL, the common scheduling policy)
+#				tasks of the same nice level will be running for, or looking at it the other way around, the longest duration two tasks 
+#				of the same nice level will be delayed for. When a task requests cpu time, it is given a quota (time_slice) equal to the 
+#				rr_interval and a virtual deadline, while increasing it will improve throughput, but at the cost of worsening latencies.
+#
+# 				/system/xbin/echo XXX > /proc/sys/kernel/rr_interval;
+
+# dirty_background_ratio: 	Contains, as a percentage of total system memory, the number of pages at which the pdflush background writeback daemon will 
+#				start writing out dirty data.
+#
+# 				/system/xbin/echo XXX > /proc/sys/vm/dirty_background_ratio;
+
+# dirty_ratio:	Contains, as a percentage of total system memory, the number of pages at which a process which is generating disk writes will 
+#				itself start writing out dirty data.
+#
+# 				/system/xbin/echo XXX > /proc/sys/vm/dirty_ratio;
+
+# iso_cpu:		Setting this to 100 is the equivalent of giving all users SCHED_RR access and setting it to 0 removes the
+#				ability to run any pseudo-realtime tasks.
+#
+# 				/system/xbin/echo XXX > /proc/sys/kernel/iso_cpu;
+
+# vfs_cache_pressure: Controls the tendency of the kernel to reclaim the memory which is used for caching of directory and inode objects.
+#				At the default value of vfs_cache_pressure = 100 the kernel will attempt to reclaim dentries and inodes at a "fair" rate with respect 
+#				to pagecache and swapcache reclaim. Decreasing vfs_cache_pressure causes the kernel to prefer to retain dentry and inode caches. 
+#				Increasing vfs_cache_pressure beyond 100 causes the kernel to prefer to reclaim dentries and inodes.
+#
+# 				/system/xbin/echo XXX > /proc/sys/vm/vfs_cache_pressure;
+
+# min_free_kbytes: This is used to force the Linux VM to keep a minimum number of kilobytes free. The VM uses this number to compute a pages_min value 
+#				for each lowmem zone in the system. Each lowmem zone gets a number of reserved free pages based proportionally on its size.
+#
+# 				/system/xbin/echo XXX > /proc/sys/vm/min_free_kbytes;
+
+# sched_latency_ns: Targeted preemption latency for CPU-bound tasks.
+#
+# 				/system/xbin/echo XXX > /proc/sys/kernel/sched_latency_ns;
+
+# sched_batch_wakeup_granularity_ns: Wake-up granularity for SCHED_BATCH.
+#
+# 				/system/xbin/echo XXX > /proc/sys/kernel/sched_batch_wakeup_granularity_ns;
+
+# sched_wakeup_granularity_ns: 	Wake-up granularity for SCHED_OTHER.
+#
+# 				/system/xbin/echo XXX > /proc/sys/kernel/sched_wakeup_granularity_ns;
+
+# sched_compat_yield: 		Applications depending heavily on sched_yield()'s behavior can expect varied performance because of the way CFS changes this, 
+#				so turning on the sysctls is recommended.
+#
+# 				/system/xbin/echo XXX > /proc/sys/kernel/sched_compat_yield;
+
+# sched_child_runs_first: 	The child is scheduled next after fork; it's the default. If set to 0, then the parent is given the baton.
+#
+# 				/system/xbin/echo XXX > /proc/sys/kernel/sched_child_runs_first;
+
+# sched_min_granularity_ns:	Minimum preemption granularity for CPU-bound tasks.
+#
+# 				/system/xbin/echo XXX > /proc/sys/kernel/sched_min_granularity_ns;
+
+# sched_features: NO_NEW_FAIR_SLEEPERS is something that will turn the scheduler into a more classic fair scheduler ?!?
+#
+# 				/system/xbin/echo NO_NORMALIZED_SLEEPER > /sys/kernel/debug/sched_features;
+
+# sched_stat_granularity_ns: Granularity for collecting scheduler statistics. [1/0]
+#
+# 				/system/xbin/echo XXX  > /proc/sys/kernel/sched_stat_granularity_ns;
+
+# sched_rt_period_us: The default values for sched_rt_period_us (1000000 or 1s) and sched_rt_runtime_us (950000 or 0.95s).  
+#				This gives 0.05s to be used by SCHED_OTHER (non-RT tasks). These defaults were chosen so that a run-away realtime 
+#				tasks will not lock up the machine but leave a little time to recover it. By setting runtime to -1 you get the old behaviour back.
+
+# threads-max: Gets/sets the limit on the maximum number of running threads system-wide.
+#
+# 				/system/xbin/echo XXX > /proc/sys/kernel/threads-max;
+
+# msgmni: 		The msgmni tunable specifies the maximum number of system-wide System V IPC message queue 
+#				identifiers (one per queue).
+#
+# 				/system/xbin/echo XXX > /proc/sys/kernel/msgmni;
+
+# sem: 			This file contains 4 numbers defining limits for System V IPC semaphores. These fields are, in order:
+#
+# 				SEMMSL - the maximum number of semaphores per semaphore set.
+# 				SEMMNS - a system-wide limit on the number of semaphores in all semaphore sets.
+# 				SEMOPM - the maximum number of operations that may be specified in a semop(2) call.
+# 				SEMMNI - a system-wide limit on the maximum number of semaphore identifiers.
+#
+# 				The default values are "250 32000 32 128".
+#
+# 				/system/xbin/echo XXX XXX XXX XXX > /proc/sys/kernel/sem;
