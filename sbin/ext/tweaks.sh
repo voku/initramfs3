a3968fca8279639af3ce4100c82b5f553bae9317
Dorimanx <yuri@bynet.co.il>
2012-04-20 15:19:52 +0300

    Revert and sync with CM9 latest commit. Now All ok.

--- sbin/ext/tweaks.sh
+++ sbin/ext/tweaks.sh
@@ -4,7 +4,7 @@
 
 #thanks to pikachu01@XDA ,Dorimanx@XDA and ELITE Developer Gokhanmoral
 /sbin/busybox sh /sbin/ext/thunderbolt.sh
-mount -o remount,rw,noatime,nodiratime,nodev,nobh,nouser_xattr,inode_readahead_blks=10,discard,barrier=0,commit=60,noauto_da_alloc,delalloc /cache /cache;
-mount -o remount,rw,noatime,nodiratime,nodev,nobh,nouser_xattr,inode_readahead_blks=10,discard,barrier=0,commit=60,noauto_da_alloc,delalloc /data /data;
+mount -o remount,rw,noatime,nodiratime,nodev,nobh,nouser_xattr,inode_readahead_blks=0,discard,barrier=0,commit=60,noauto_da_alloc,delalloc /cache /cache;
+mount -o remount,rw,noatime,nodiratime,nodev,nobh,nouser_xattr,inode_readahead_blks=0,discard,barrier=0,commit=60,noauto_da_alloc,delalloc /data /data;
 mount -o remount,rw,noatime,nodiratime /system /system
 
