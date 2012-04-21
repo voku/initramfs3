4015f54376a9bf381d932212eed8daac58f899fa
Dorimanx <yuri@bynet.co.il>
2012-04-21 03:14:28 +0300

    Added new name for my tweaks, and sync with voku for some tweaks.

--- sbin/ext/tweaks.sh
+++ sbin/ext/tweaks.sh
@@ -3,7 +3,7 @@
 # some parameters are taken from http://forum.xda-developers.com/showthread.php?t=1292743 (highly recommended to read)
 
 #thanks to pikachu01@XDA ,Dorimanx@XDA and ELITE Developer Gokhanmoral
-/sbin/busybox sh /sbin/ext/thunderbolt.sh
+/sbin/busybox sh /sbin/ext/cortexbrain-tune.sh
 mount -o remount,rw,noatime,nodiratime,nodev,nobh,nouser_xattr,inode_readahead_blks=0,discard,barrier=0,commit=60,noauto_da_alloc,delalloc /cache /cache;
 mount -o remount,rw,noatime,nodiratime,nodev,nobh,nouser_xattr,inode_readahead_blks=0,discard,barrier=0,commit=60,noauto_da_alloc,delalloc /data /data;
 mount -o remount,rw,noatime,nodiratime /system /system
