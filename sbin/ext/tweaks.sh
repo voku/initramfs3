b239e7aa2c10df0e23db51f7e63b0f029a2b229a
Dorimanx <yuri@bynet.co.il>
2012-04-29 21:49:53 +0300

    Improved my last commit about data=writeback for data/cache

--- sbin/ext/tweaks.sh
+++ sbin/ext/tweaks.sh
@@ -4,23 +4,9 @@
 
 #thanks to pikachu01@XDA ,Dorimanx@XDA and ELITE Developer Gokhanmoral
 
-#Stoping VM
-stop
-
-#Unmounting CACHE
-umount -l /cache
-
-#setting writeback jornal
-/sbin/tune2fs -o journal_data_writeback /res/dev/cache
-
-#Running mount check before mount
-/sbin/e2fsck -p /res/dev/cache
 
 #Mounting and tweaking.
-mount -t ext4 -o rw,noatime,nodiratime,nodev,nobh,nouser_xattr,inode_readahead_blks=0,discard,barrier=0,commit=30,noauto_da_alloc,delalloc /res/dev/cache /cache;
+mount -o remount,rw,noatime,nodiratime,nodev,nobh,nouser_xattr,inode_readahead_blks=0,discard,barrier=0,commit=30,noauto_da_alloc,delalloc /res/dev/cache /cache;
 mount -o remount,rw,noatime,nodiratime,nodev,nobh,nouser_xattr,inode_readahead_blks=0,discard,barrier=0,commit=30,noauto_da_alloc,delalloc /data /data;
-mount -o remount,rw,noatime,nodiratime /system /system
-
-#Starting VM
-start
+mount -o remount,rw,noatime,nodiratime,inode_readahead_blks=0 /system /system
 
