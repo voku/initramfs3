6f32a2604035f1f38a92a6b8b55a4483cb7db03b
Lars Moelleken <voku@voku-online.de>
2012-05-07 04:04:20 +0200

    + merge tweak_lite.sh & cortexbrain-tune.sh => cortexbrain-tune.sh

--- sbin/ext/tweaks.sh
+++ sbin/ext/tweaks.sh
@@ -6,7 +6,10 @@
 
 
 #Mounting and tweaking.
+for l in $(busybox mount | grep ext[3-4] | cut -d " " -f3);
+do
+	mount -o remount,noatime,nodiratime,inode_readahead_blks=0,barrier=0 $l;
+done;
 mount -o remount,rw,noatime,nodiratime,nodev,nobh,nouser_xattr,inode_readahead_blks=0,discard,barrier=0,commit=60,noauto_da_alloc,delalloc /cache;
 mount -o remount,rw,noatime,nodiratime,nodev,nobh,nouser_xattr,inode_readahead_blks=0,discard,barrier=0,commit=60,noauto_da_alloc,delalloc /data;
-mount -o remount,rw,noatime,nodiratime,inode_readahead_blks=0,barrier=0 /system
 
