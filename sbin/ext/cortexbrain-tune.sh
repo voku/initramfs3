5c04d400658445a6259d4bdfc5d7ac57ab589469
root <root@voku-VirtualBox.(none)>
2012-06-05 19:57:58 +0200

    + fix merge

--- sbin/ext/cortexbrain-tune.sh
+++ sbin/ext/cortexbrain-tune.sh
@@ -188,10 +188,7 @@ setprop mot.proximity.delay 15
 # more Tweaks
 setprop dalvik.vm.execution-mode int:jit
 setprop persist.adb.notify 0
-<<<<<<< HEAD
-=======
 echo NO_GENTLE_FAIR_SLEEPERS > /sys/kernel/debug/sched_features
->>>>>>> Dorimanx-SG2-I9100-initramfs3/master
 
 # =========
 # BATTERY-TWEAKS
