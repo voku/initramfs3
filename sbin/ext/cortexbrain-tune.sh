7e531c5ad5a3d86fde2bc3b6f316cfcf1b4295ef
Lars Moelleken <voku@voku-online.de>
2012-06-07 16:57:23 +0200

    + ...

--- sbin/ext/cortexbrain-tune.sh
+++ sbin/ext/cortexbrain-tune.sh
@@ -561,7 +561,7 @@ else
 	echo "$scaling_governor" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor;
 	echo "$scaling_max_freq" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq;
 
-	# value from settins
+	# value from settings
 	echo "$sched_mc_power_savings" /sys/devices/system/cpu/sched_mc_power_savings
 
 	# CPU scheduler
