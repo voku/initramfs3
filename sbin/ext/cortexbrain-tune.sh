c9286e64c5be95a06ee477779e761ac5fa1edfab
root <root@voku-VirtualBox.(none)>
2012-06-08 11:56:39 +0200

    + white space clean.

--- sbin/ext/cortexbrain-tune.sh
+++ sbin/ext/cortexbrain-tune.sh
@@ -527,9 +527,9 @@ if [ $CHARGING -ge 1 ]; then
 		# For this to work you need CONFIG_SCHED_DEBUG=y set in kernel settings.
 		if [ -e /proc/sys/kernel/sched_latency_ns ]; then
 			# CFS
-    			echo "10000000" > /proc/sys/kernel/sched_latency_ns;
-            		echo "1000000" > /proc/sys/kernel/sched_wakeup_granularity_ns;
-    			echo "800000" > /proc/sys/kernel/sched_min_granularity_ns;
+			echo "10000000" > /proc/sys/kernel/sched_latency_ns;
+			echo "1000000" > /proc/sys/kernel/sched_wakeup_granularity_ns;
+			echo "800000" > /proc/sys/kernel/sched_min_granularity_ns;
 			echo "-1" > /proc/sys/kernel/sched_rt_runtime_us;
 			echo "100000" > /proc/sys/kernel/sched_rt_period_us;
 		fi;
