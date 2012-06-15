976139d37ef50dd4145b6baead14bf0c45d72fdd
root <root@voku-VirtualBox.(none)>
2012-06-15 14:44:40 +0200

    + revert profile changes ...

--- sbin/ext/cortexbrain-tune.sh
+++ sbin/ext/cortexbrain-tune.sh
@@ -35,7 +35,7 @@ MEMORY_TWEAKS_ENABLED=1;
 TCP_TWEAKS_ENABLED=1;
 RIL_TWEAKS_ENABLED=0;
 FIREWALL_TWEAKS_ENABLED=1;
-BACKGROUND_PROCESS_ENABLED=0;
+BACKGROUND_PROCESS_ENABLED=1;
 
 # Static sets for functions, they will be changes by other functions later.
 # TODO: Add DEFAULT_BALANCE for Default profile now it's=MORE_BATTERY=1
