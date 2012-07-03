8d8224719783051d2cfd73ce58271587fb8828e4
Lars Moelleken <voku@voku-online.de>
2012-07-03 21:00:20 +0200

    + added "scaling_min_freq" ...

--- sbin/ext/cortexbrain-tune.sh
+++ sbin/ext/cortexbrain-tune.sh
@@ -335,7 +335,7 @@ if [ $MORE_BATTERY == 1 ]; then
 		echo "1" > /sys/devices/system/cpu/cpufreq/hyper/sampling_down_factor;
 		echo "1" > /sys/devices/system/cpu/cpufreq/hyper/down_differential;
 		echo "160000" > /sys/devices/system/cpu/cpufreq/hyper/sampling_rate;
-                echo "${scaling_min_suspend_freq}" > /sys/devices/system/cpu/cpufreq/hyper/suspend_freq
+		echo "${scaling_min_suspend_freq}" > /sys/devices/system/cpu/cpufreq/hyper/suspend_freq
 		echo "20" > /sys/devices/system/cpu/cpufreq/hyper/freq_step
 	fi;
 
@@ -714,8 +714,9 @@ if [ $CHARGING -ge 1 ]; then
 
 else
 
-	# set governor
-        echo "${scaling_governor}" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor;
+	# set governor & CPU speed
+	echo "${scaling_governor}" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor;
+	echo "${scaling_min_freq}" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq;
 	echo "${scaling_max_freq}" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq;
 
 	# cpu - hotplug=1 core online second on demand
@@ -804,8 +805,9 @@ else
 	MODE="SLEEP";
 fi;
 
-# Reduce CPU max Speed to 800Mhz
-echo "1000000" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq;
+# Reduce CPU speed
+echo "${scaling_min_suspend_freq}" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq;
+echo "${scaling_max_suspend_freq}" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq;
 
 # cpu - second core always-off
 echo "off" > /sys/devices/virtual/misc/second_core/hotplug_on;
