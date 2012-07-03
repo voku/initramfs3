6a171d97ba9cb01f901b4d2906948294fcf6bf46
Dorimanx <yuri@bynet.co.il>
2012-07-04 02:48:59 +0300

    Added Wakeup booster for users that set max cpu below 1.2Ghz and stuck at screen lock. Now it's will give them 1.2/1.5Ghz for 5 sec, then do all the changes. And drop back to user setting.

--- sbin/ext/cortexbrain-tune.sh
+++ sbin/ext/cortexbrain-tune.sh
@@ -684,8 +684,17 @@ fi;
 AWAKE_MODE()
 {
 
-# Restore Smooth Level
-kmemhelper -n smooth_level -o 0 -t int ${smooth_level0}
+# Awake booster!
+MAX_CPU_SPEED=`cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq`
+if [ $MAX_CPU_SPEED -gt 1200000 ]; then
+	echo "performance" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor;
+else
+	echo "performance" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor;
+	echo "1000000" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq;
+	echo "1200000" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq > /dev/null 2>&1;
+	echo "1500000" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq > /dev/null 2>&1;
+fi;
+sleep 5
 
 # charging & screen is on
 CHARGING=`cat /sys/class/power_supply/battery/charging_source`;
@@ -740,6 +749,9 @@ else
 
 	MODE="AWAKE";
 fi;
+
+# Restore Smooth Level
+kmemhelper -n smooth_level -o 0 -t int ${smooth_level0}
 
 if [ $BATTERY_TWEAKS_ENABLED == 1 ]; then
 	BATTERY_TWEAKS;
