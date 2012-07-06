acaa662216ed5d0c13560f4cf8531d3b537dae2e
Lars Moelleken <voku@voku-online.de>
2012-07-06 03:03:41 +0200

    + ...

--- sbin/ext/cortexbrain-tune.sh
+++ sbin/ext/cortexbrain-tune.sh
@@ -702,13 +702,14 @@ AWAKE_MODE()
 {
 
 # Awake booster!
-# Kill the wakeup bug!
+# kill the wakeup bug! boost the CPU to MAX allowed
 echo "performance" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor;
 
 echo "1000000" > /sys/devices/virtual/sec/sec_touchscreen/tsp_touch_freq;
 echo "1200000" > /sys/devices/virtual/sec/sec_touchscreen/tsp_touch_freq > /dev/null 2>&1;
 echo "1500000" > /sys/devices/virtual/sec/sec_touchscreen/tsp_touch_freq > /dev/null 2>&1;
 
+# now boost the screen lock freq to Max allowed
 echo "1000000" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq;
 echo "1200000" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq > /dev/null 2>&1;
 echo "1500000" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq > /dev/null 2>&1;
