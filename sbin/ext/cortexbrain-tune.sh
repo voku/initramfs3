18fd84d32a1961886d38426bc22d2d7b3b4d1b12
Lars Moelleken <lmoelleken@menadwork.com>
2012-08-06 18:05:17 +0200

    + set "supplicant_scan_interval" only to srceen-off-value if the settings-value is lower ... ;)

--- sbin/ext/cortexbrain-tune.sh
+++ sbin/ext/cortexbrain-tune.sh
@@ -788,7 +788,9 @@ if [ $cortexbrain_cpu == on ]; then
 fi;
 
 # set wifi.supplicant_scan_interval
-setprop wifi.supplicant_scan_interval 180;
+if [ $supplicant_scan_interval -lt 180 ]; then
+	setprop wifi.supplicant_scan_interval 180;
+fi;
 
 # Bus Freq for deep sleep
 echo "30" > /sys/devices/system/cpu/cpufreq/busfreq_up_threshold;
