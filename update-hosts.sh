161f6688114db08844982481fd7de1355b9910c7
Dorimanx <yuri@bynet.co.il>
2012-09-02 13:58:23 +0300

    Fixed HOSTS update and compress scripts.

--- update-hosts.sh
+++ update-hosts.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-xzcat res/misc/hosts.tar.xz > res/misc/hosts;
-mv res/misc/hosts /tmp/;
+xzcat res/misc/hosts.xz > /tmp/hosts;
 wget http://winhelp2002.mvps.org/hosts.zip;
 unzip hosts.zip HOSTS;
 mv HOSTS res/misc/hosts;
@@ -9,7 +8,7 @@ rm hosts.zip;
 
 diff /tmp/hosts res/misc/hosts;
 echo "<- changes";
-rm -f res/misc/hosts.tar.xz;
+rm -f res/misc/hosts.xz;
 xz -zekv9 res/misc/hosts;
 rm -f res/misc/hosts;
 echo "compressed new hosts file";
