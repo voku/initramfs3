5725570294177235f09f5708dc66de7d74ab8357
Dorimanx <yuri@bynet.co.il>
2012-09-02 12:43:11 +0300

    Compressed HOSTS file with XZ and tuned the host update file for DEVS only!

--- update-hosts.sh
+++ update-hosts.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-mv ./res/misc/hosts /tmp/;
+xzcat res/misc/hosts.tar.xz > res/misc/hosts;
+mv res/misc/hosts /tmp/;
 wget http://winhelp2002.mvps.org/hosts.zip;
 unzip hosts.zip HOSTS;
-mv HOSTS ./res/misc/hosts;
+mv HOSTS res/misc/hosts;
 rm hosts.zip;
 
-diff /tmp/hosts ./res/misc/hosts;
+diff /tmp/hosts res/misc/hosts;
 echo "<- changes";
+rm -f res/misc/hosts.tar.xz;
+tar -cvJ --xz res/misc/hosts > res/misc/hosts.tar.xz;
+rm -f res/misc/hosts;
+echo "compressed new hosts file";
+
