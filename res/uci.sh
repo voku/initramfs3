ad568be591223b921a322ddddfee28cb2fa5ffd3
Lars Moelleken <voku@voku-online.de>
2012-09-18 05:16:56 +0200

    + for me this looks better ;) but I can't start "ExTweaks"-App anymore :( :( -> "No compatible interfaces detected"

--- res/uci.sh
+++ res/uci.sh
@@ -6,7 +6,6 @@
 # You'll need to modify the files in /res/customconfig directory
 
 ACTION_SCRIPTS=/res/customconfig/actions;
-LOCK_FILE=/data/.siyah/uci_running;
 source /res/customconfig/customconfig-helper;
 
 # first, read defaults
@@ -18,44 +17,24 @@ read_config;
 case "${1}" in
 	rename)
     	rename_profile "${2}" "${3}";
-    	exit 0;
     ;;
 	delete)
     	delete_profile "${2}";
-    	exit 0;
     ;;
 	select)
     	select_profile "${2}";
-    	exit 0;
     ;;
 	config)
     	print_config;
-		exit 0;
     ;;
 	list)
     	list_profile;
-		exit 0;
     ;;
 	apply)
 		apply_config;
-		echo "uci done" > $LOCK_FILE;
-	;;
-	restore)
-		apply_config;
-		exit 0;
 	;;
 	*)
     	. ${ACTION_SCRIPTS}/${1} ${1} ${2} ${3} ${4} ${5} ${6};
-		echo "uci done" > $LOCK_FILE;
+		write_config;
 	;;
 esac;
-
-# write back the config to the active profile
-while [ ! -e $LOCK_FILE ]; do
-	sleep 5;
-	echo "Still waiting till all DONE!";
-done;
-
-write_config;
-
-rm -f $LOCK_FILE;
