8e571e22a06c2b0904ea498a0dfde7a61e353052
Yuri.Sh <yuri@bynet.co.il>
2012-10-20 13:39:10 +0200

    New Patched JB SQL 3.7.11 with Sync reduced function! +300% speed! Now SYNC will be done at the end of operation, and not on each injection.
    i will upload SOURCE diff to my sql repo in GIT.

--- compress-sql.sh
+++ compress-sql.sh
@@ -12,9 +12,17 @@ fi;
 
 if [ -e res/misc/sql/jb/libsqlite.so ]; then
 	rm -f res/misc/sql/jb/libsqlite.so.xz
-	chmod 755 res/misc/sql/ics/libsqlite.so
+	chmod 755 res/misc/sql/jb/libsqlite.so
 	xz -zekv9 res/misc/sql/jb/libsqlite.so
 	rm -f res/misc/sql/jb/libsqlite.so
-	echo "JB sql compressed"
+	echo "JB lib sql compressed"
+fi;
+
+if [ -e res/misc/sql/jb/sqlite3 ]; then
+	rm -f res/misc/sql/jb/sqlite3.xz
+	chmod 755 res/misc/sql/jb/sqlite3
+	xz -zekv9 res/misc/sql/jb/sqlite3
+	rm -f res/misc/sql/jb/sqlite3
+	echo "JB xbin sql compressed"
 fi;
 
