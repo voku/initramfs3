ff9bb28f92253eaf5b3fb243ac6d3a0eb452b136
Lars Moelleken <voku@voku-online.de>
2012-10-31 03:16:16 +0100

    + some testing ...

--- sbin/ext/cortexbrain-tune.sh
+++ sbin/ext/cortexbrain-tune.sh
@@ -445,70 +445,15 @@ FIREWALL_TWEAKS()
 		echo "1" > /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts;
 		echo "1" > /proc/sys/net/ipv4/icmp_echo_ignore_all;
 		echo "1" > /proc/sys/net/ipv4/icmp_ignore_bogus_error_responses;
-		echo "1" > /proc/sys/net/ipv6/icmp_echo_ignore_broadcasts;
-		echo "1" > /proc/sys/net/ipv6/icmp_echo_ignore_all;
-		echo "1" > /proc/sys/net/ipv6/icmp_ignore_bogus_error_responses;
-
-		# syn protection
-		if [ -e /proc/sys/net/ipv4/tcp_synack_retries ]; then
-			echo "10" > /proc/sys/net/ipv4/tcp_synack_retries;
-		fi;
-
-		if [ -e /proc/sys/net/ipv6/tcp_synack_retries ]; then
-			echo "10" > /proc/sys/net/ipv6/tcp_synack_retries;
-		fi;
-
-		if [ -e /proc/sys/net/ipv6/tcp_syncookies ]; then
-			echo "0" > /proc/sys/net/ipv6/tcp_syncookies;
-		fi;
-
-		if [ -e /proc/sys/net/ipv4/tcp_syncookies ]; then
-			echo "1" > /proc/sys/net/ipv4/tcp_syncookies;
-		fi;
-
-		if [ -e /proc/sys/net/ipv4/tcp_max_syn_backlog ]; then
-			echo "4096" > /proc/sys/net/ipv4/tcp_max_syn_backlog;
-		fi;
-
-		# IPv6 privacy tweak
-		echo "2" > /proc/sys/net/ipv6/conf/all/use_tempaddr;
 
 		# drop spoof, redirects, etc
-		echo "1" > /proc/sys/net/ipv4/conf/all/rp_filter;
-		echo "1" > /proc/sys/net/ipv4/conf/default/rp_filter;
-		echo "0" > /proc/sys/net/ipv4/conf/all/send_redirects;
-		echo "0" > /proc/sys/net/ipv4/conf/default/send_redirects;
-		echo "0" > /proc/sys/net/ipv4/conf/default/accept_redirects;
-		echo "0" > /proc/sys/net/ipv4/conf/all/accept_source_route;
-		echo "0" > /proc/sys/net/ipv4/conf/default/accept_source_route;
-
-		if [ -e /proc/sys/net/ipv6/conf/all/rp_filter ]; then
-			echo "1" > /proc/sys/net/ipv6/conf/all/rp_filter;
-		fi;
-
-		if [ -e /proc/sys/net/ipv6/conf/default/rp_filter ]; then
-			echo "1" > /proc/sys/net/ipv6/conf/default/rp_filter;
-		fi;
-
-		if [ -e /proc/sys/net/ipv6/conf/all/send_redirects ]; then
-			echo "0" > /proc/sys/net/ipv6/conf/all/send_redirects;
-		fi;
-
-		if [ -e /proc/sys/net/ipv6/conf/default/send_redirects ]; then
-			echo "0" > /proc/sys/net/ipv6/conf/default/send_redirects;
-		fi;
-
-		if [ -e /proc/sys/net/ipv6/conf/default/accept_redirects ]; then
-			echo "0" > /proc/sys/net/ipv6/conf/default/accept_redirects;
-		fi;
-
-		if [ -e /proc/sys/net/ipv6/conf/all/accept_source_route ]; then
-			echo "0" > /proc/sys/net/ipv6/conf/all/accept_source_route;
-		fi;
-
-		if [ -e /proc/sys/net/ipv6/conf/default/accept_source_route ]; then
-			echo "0" > /proc/sys/net/ipv6/conf/default/accept_source_route;
-		fi;
+		#echo "1" > /proc/sys/net/ipv4/conf/all/rp_filter;
+		#echo "1" > /proc/sys/net/ipv4/conf/default/rp_filter;
+		#echo "0" > /proc/sys/net/ipv4/conf/all/send_redirects;
+		#echo "0" > /proc/sys/net/ipv4/conf/default/send_redirects;
+		#echo "0" > /proc/sys/net/ipv4/conf/default/accept_redirects;
+		#echo "0" > /proc/sys/net/ipv4/conf/all/accept_source_route;
+		#echo "0" > /proc/sys/net/ipv4/conf/default/accept_source_route;
 
 		log -p i -t $FILE_NAME "*** firewall-tweaks ***: enabled";
 	fi;
