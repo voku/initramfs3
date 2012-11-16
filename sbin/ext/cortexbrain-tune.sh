b8496bd42a81b92a6e60c51d26b532cf69a61cec
Yuri.Sh <yuri@bynet.co.il>
2012-11-17 01:47:04 +0200

    Updates from JB SAMMY KERNEL + modds to my INIT and cortex.

--- sbin/ext/cortexbrain-tune.sh
+++ sbin/ext/cortexbrain-tune.sh
@@ -392,7 +392,7 @@ MEMORY_TWEAKS()
 		echo "50" > /proc/sys/vm/overcommit_ratio; # default: 50
 		echo "128 128" > /proc/sys/vm/lowmem_reserve_ratio;
 		echo "3" > /proc/sys/vm/page-cluster; # default: 3
-		echo "16384" > /proc/sys/vm/min_free_kbytes;
+		echo "8192" > /proc/sys/vm/min_free_kbytes;
 
 		log -p i -t $FILE_NAME "*** MEMORY_TWEAKS ***: enabled";
 	fi;
