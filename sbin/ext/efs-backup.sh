2e2af80ce18d0d8d92bb3a8aeea90ed3978a59e1
Yuri.Sh <yuri@bynet.co.il>
2013-03-01 15:22:01 +0200

    Fixed Boot Anim for JB SAMMY custom roms + other fixing. Some parts from GM Recovery not pushed to git changes.

--- sbin/ext/efs-backup.sh
+++ sbin/ext/efs-backup.sh
@@ -8,8 +8,12 @@ if [ ! -f /data/.siyah/efsbackup.tar.gz ]; then
 	$BB tar zcvf /data/.siyah/efsbackup.tar.gz /efs;
 	$BB cat /dev/block/mmcblk0p1 > /data/.siyah/efsdev-mmcblk0p1.img;
 	$BB gzip /data/.siyah/efsdev-mmcblk0p1.img;
-	$BB cp /data/.siyah/efs* /data/media;
+	$BB cp /data/.siyah/efs* /data/media/;
 	$BB chmod 777 /data/media/efsdev-mmcblk0p3.img;
 	$BB chmod 777 /data/media/efsbackup.tar.gz;
+	(
+		sleep 120;
+		cp /data/media/efs* /sdcard/;
+	)&
 fi;
 
