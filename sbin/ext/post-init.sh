3d1e6265595e411511d380a8d34af6814d872c7a
Yuri.Sh <yuri@bynet.co.il>
2013-04-03 16:03:03 +0300

    Deleted ICS JUNK! + Updated CM10.1 INIT and cleaned JUNK!

--- sbin/ext/post-init.sh
+++ sbin/ext/post-init.sh
@@ -82,8 +82,8 @@ $BB chmod -R 755 /lib;
 (
 	sleep 50;
 	# order of modules load is important
-	$BB insmod /lib/modules/j4fs.ko;
-	$BB mount -t j4fs /dev/block/mmcblk0p4 /mnt/.lfs
+#	$BB insmod /lib/modules/j4fs.ko;
+#	$BB mount -t j4fs /dev/block/mmcblk0p4 /mnt/.lfs
 	$BB insmod /lib/modules/Si4709_driver.ko;
 
 	if [ "$usbserial_module" == "on" ]; then
