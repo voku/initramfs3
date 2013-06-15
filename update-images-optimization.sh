e5970e7b4322ef5622f7e7759b8a71f9afec5b2f
Yuri.Sh <yuri@bynet.co.il>
2013-06-15 20:08:38 +0300

    Now it's safe to use update-images-optimization.sh, recovery OK.
    Optimized all png files.

--- update-images-optimization.sh
+++ update-images-optimization.sh
@@ -9,15 +9,25 @@
 # aptitude install optipng pngcrush jpegoptim
 #############################################
 
+# protect recovery images from modding, or recovery will be broken.
+mv res/images/icon_installing*.png /tmp/;
+mv res/images/indeterminate*.png /tmp/;
+mv res/images/progress_*.png /tmp/;
+
 find . -iname '*.png' -exec optipng -o7 {} \;
 
 for file in `find . -name "*.png"`;do
-   echo $file;
-   pngcrush -rem alla -reduce -brute "$file" tmp_img_file.png;
-   mv -f tmp_img_file.png $file;
+	echo $file;
+	pngcrush -rem alla -reduce -brute "$file" tmp_img_file.png;
+	mv -f tmp_img_file.png $file;
 done;
 
 find . -iname '*.jpg' -exec jpegoptim --force {} \;
+
+# restore recovery images type.
+mv /tmp/icon_installing*.png res/images/;
+mv /tmp/indeterminate*.png res/images/;
+mv /tmp/progress_*.png res/images/;
 
 echo "done, picks optimized"
 
