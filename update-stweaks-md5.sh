3f1881f5be276394d8dc737c1e90f9eb1ba1199a
Lars Moelleken <voku@voku-online.de>
2013-06-16 12:50:58 +0200

    Revert "+ full sync ..."
    This reverts commit 1ae580661795a2ebb503152c68f87d6b052bd033.

--- update-stweaks-md5.sh
+++ update-stweaks-md5.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-md5sum res/misc/payload/STweaks.apk | awk '{print $1}' > res/stweaks_md5;
-chmod 644 res/stweaks_md5;
-cat res/stweaks_md5;
+
+MD5FILE="res/stweaks_md5";
+
+md5sum res/misc/payload/STweaks.apk | awk '{print $1}' > $MD5FILE;
+stat $MD5FILE || exit 1;
+chmod 644 $MD5FILE;
+cat $MD5FILE;
 
