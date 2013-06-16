f76e8ee0a2d5dcb675118a875a7c4632d2d5e173
Yuri.Sh <yuri@bynet.co.il>
2013-06-17 00:25:10 +0300

    Added small script to fix DATA partition manually via RECOVERY! using ADB.
    Now Advanced Users and DEVS including me, can easy fix the broken data
    partition.
    
    Please DO DATA backup before, this can kill some DATA files if they broken
    and cant be fixed.

---
+++ sbin/ext/fix_my_data.sh
@@ -0,0 +1,19 @@
+#!/sbin/busybox sh
+
+read -t 5 -p "Start fixing DATA partition? 5sec time out then NO (y/n)"
+if [ "$REPLY" == "y" ]; then
+	if [ ! -e /data/dalvik_cache/ ]; then
+		echo "starting to fix DATA"
+		/sbin/e2fsck -fyc /dev/block/mmcblk0p10;
+		/sbin/e2fsck -fyc /dev/block/mmcblk0p10;
+		/sbin/e2fsck -p /dev/block/mmcblk0p10;
+		mount -t ext4 /dev/block/mmcblk0p10 /data;
+		rm -f /data/dalvik_cache/*;
+		sync;
+		umount /data;
+		echo "All done!"
+	else
+		echo "DATA Partition is MOUNTED! you can't run this script, reboot to recovery, them run via ADB"
+	fi;
+fi;
+
