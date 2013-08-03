70d6ac03dde6a3b4fc1ee4b3a204a824eb09d262
alucard24 <dmaboh2@gmail.com>
2013-08-03 15:32:02 +0200

    Corrected empty external sdcard if we don't use a sdcard with exfat partition!

--- sbin/ext/system_tune_on_init.sh
+++ sbin/ext/system_tune_on_init.sh
@@ -51,15 +51,14 @@ SDCARD_FIX()
 	$BB echo "FIXING STORAGE" >> $LOG_SDCARDS;
 
 	if [ -e /dev/block/mmcblk1p1 ]; then
-		if [ `cat /tmp/sammy_rom` -eq "0" ]; then
-			$BB mount -t exfat /dev/block/mmcblk1p1 /storage/sdcard1;
-		else
-			$BB mount -t exfat /dev/block/mmcblk1p1 /storage/extSdCard;
-		fi;
-
 		chmod 777 /proc/self/mounts;
 		EXFAT_CHECK=`cat /proc/self/mounts | grep "/dev/block/mmcblk1p1" | wc -l`;
 		if [ "$EXFAT_CHECK" -eq "1" ]; then
+			if [ `cat /tmp/sammy_rom` -eq "0" ]; then
+				$BB mount -t exfat /dev/block/mmcblk1p1 /storage/sdcard1;
+			else
+				$BB mount -t exfat /dev/block/mmcblk1p1 /storage/extSdCard;
+			fi;
 			$BB echo "EXTERNAL SDCARD CHECK" >> $LOG_SDCARDS;
 			cp /sbin/libexfat_utils.so /system/lib/;
 			/sbin/fsck.exfat -R /dev/block/mmcblk1p1 >> $LOG_SDCARDS;
